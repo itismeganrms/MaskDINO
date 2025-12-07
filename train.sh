0e72b50148cba2497f6d9b34845bb08050e71bd2
Megan Mirnalini Sundaram R <mrnalini@live.in>
2025-12-07 19:32:42 +0100

    Updating shell script for new dataset and runs

--- train.sh
+++ train.sh
@@ -2,11 +2,9 @@
 
 #SBATCH --job-name=maskdino
 #SBATCH --time=12:00:00
-#SBATCH --partition=gpu-long
+#SBATCH --partition=gpu-2080ti-11g
 #SBATCH --output=/home/mrajaraman/slurm/maskdino/train/output-%A.out
 #SBATCH --gres=gpu:1
-#SBATCH --constraint="A100.4g.40gb|A100.3g.40gb"
-
 
 echo "## Starting GPU test on $HOSTNAME"
 module purge
@@ -28,13 +26,34 @@ python --version
 TILE_SIZE=512
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
-python train_net.py --num-gpus 1 \
---resume \
---exp_id ${TILE_SIZE} \
---train_iter 15000 \
---config-file /home/mrajaraman/master-thesis-dragonfly/external/maskdino-dragonfly/configs/lifeplan/instance-segmentation/maskdino_R50_bs16_50ep_3s_dowsample1_2048.yaml \
---dataset_path /home/mrajaraman/dataset/coco-roboflow/ \
-OUTPUT_DIR output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
-DATASETS.TRAIN "(\"dragonfly_${TILE_SIZE}_train\",)" \
-DATASETS.TEST "(\"dragonfly_${TILE_SIZE}_valid\",)"  \
+# ---- TRAINING COMMANDS FOR ORIGINAL DATASET BELOW ----
+# python train_net.py --num-gpus 1 \
+# --exp_id ${TILE_SIZE} \
+# --train_iter 2500 \
+# --config-file /home/mrajaraman/master-thesis-dragonfly/external/maskdino-dragonfly/configs/lifeplan/instance-segmentation/maskdino_R50_bs16_50ep_3s_dowsample1_2048.yaml \
+# --dataset_path /home/mrajaraman/dataset/coco-roboflow/ \
+# OUTPUT_DIR output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
+# DATASETS.TRAIN "(\"dragonfly_${TILE_SIZE}_train\",)" \
+# DATASETS.TEST "(\"dragonfly_${TILE_SIZE}_valid\",)"  \
 # MODEL.WEIGHTS /h/jquinto/MaskDINO/maskdino_r50_50ep_300q_hid2048_3sd1_instance_maskenhanced_mask46.3ap_box51.7ap.pth \
+
+# python train_net.py --num-gpus 1 \
+# --exp_id ${TILE_SIZE} \
+# --train_iter 5000 \
+# --config-file /home/mrajaraman/master-thesis-dragonfly/external/maskdino-dragonfly/configs/lifeplan/instance-segmentation/maskdino_R50_bs16_50ep_3s_dowsample1_2048.yaml \
+# --dataset_path /home/mrajaraman/dataset/coco-roboflow/ \
+# OUTPUT_DIR output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
+# DATASETS.TRAIN "(\"dragonfly_${TILE_SIZE}_train\",)" \
+# DATASETS.TEST "(\"dragonfly_${TILE_SIZE}_valid\",)"  \
+
+# python train_net.py --num-gpus 1 \
+# --exp_id ${TILE_SIZE} \
+# --train_iter 20000 \
+# --config-file /home/mrajaraman/master-thesis-dragonfly/external/maskdino-dragonfly/configs/lifeplan/instance-segmentation/maskdino_R50_bs16_50ep_3s_dowsample1_2048.yaml \
+# --dataset_path /home/mrajaraman/dataset/coco-merged/ \
+# OUTPUT_DIR output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
+# DATASETS.TRAIN "(\"dragonfly_${TILE_SIZE}_train\",)" \
+# DATASETS.TEST "(\"dragonfly_${TILE_SIZE}_valid\",)"  \
+# # MODEL.WEIGHTS /h/jquinto/MaskDINO/maskdino_r50_50ep_300q_hid2048_3sd1_instance_maskenhanced_mask46.3ap_box51.7ap.pth \
+
+# ---- TRAINING COMMANDS FOR MERGED DATASET BELOW ----
